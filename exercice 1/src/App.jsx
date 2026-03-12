bb13a17b0d43fee7f4607b0a1a09aa5c0de61a0d
borared <boraofficial76@gmail.com>
2026-03-12 15:18:47 +0700

    Finish exercise-1

--- exercice 1/src/App.jsx
+++ exercice 1/src/App.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
+  const [score, setScore] = useState(0);
+
   const getScoreBarStyle = () => {
-    // 1- Compute width
-    const scoreWidth = `40%`;
+    const scoreWidth = `${score * 10}%`;
 
-    // 2- Compute color (optional)
-    let scoreColor = `#f3bc47`;
+    let scoreColor = "#f3bc47";
 
-    // 3 - Return the style object
+    if (score >= 4 && score <= 7) {
+      scoreColor = "#b6d957";
+    }
+
+    if (score >= 8) {
+      scoreColor = "#6cc644";
+    }
+
     return {
       width: scoreWidth,
       backgroundColor: scoreColor,
@@ -16,17 +23,21 @@ export default function App() {
   };
 
   return (
-    <>
-      <div className="score-panel">
-        <h1>My Score in React</h1>
+    <div className="score-panel">
+      <h1>My Score in React</h1>
 
-        <small>Enter a score (0 to 10): </small>
-        <input type="number" min="0" max="10"></input>
+      <small>Enter a score (0 to 10): </small>
+      <input
+        type="number"
+        min="0"
+        max="10"
+        value={score}
+        onChange={(e) => setScore(Number(e.target.value))}
+      />
 
-        <div className="score-bar">
-          <div className="score-bar-value" style={getScoreBarStyle()}></div>
-        </div>
+      <div className="score-bar">
+        <div className="score-bar-value" style={getScoreBarStyle()}></div>
       </div>
-    </>
+    </div>
   );
 }
